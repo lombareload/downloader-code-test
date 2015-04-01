9ef78d75f94fd72459f36f1fdaa6044a00ed1679
Fabián López <fabian.lopez@prodigious.com>
2015-04-01 12:28:00 -0500

    basic project structure

---
+++ src/main/java/com/fabian/downloader/App.java
@@ -0,0 +1,10 @@
+package com.fabian.downloader;
+
+public class App {
+    private ExecutorService executor;
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
